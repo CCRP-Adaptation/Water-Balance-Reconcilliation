f77ad6d57f175334e2c288dce0488cca5ecd8685
msears <megan_sears@partner.nps.gov>
2021-02-10 13:15:55 -0700

    2/10/21 updated freeze function. melt/pack loop still needs work.

--- tests/SWE_test.R
+++ tests/SWE_test.R
@@ -24,14 +24,14 @@ frog <- frog %>%
 low_thresh_temp = frog$Jenningcoeff-3 
 high_thresh_temp = frog$Jenningcoeff+3
 
-
-frog <- frog %>% #need to go back and fix this so the first time step has different equation & write as function
-  mutate(freeze = ifelse(
+get_freeze = function(low_thresh_temp, tmean, high_thresh_temp){
+  freeze = ifelse(
     tmean <= low_thresh_temp, 0, 
     ifelse(tmean >= high_thresh_temp,
-           1, (0.167*(tmean-low_thresh_temp)))))
+           1, (0.167*(tmean-low_thresh_temp))))
+}
 
-#write.csv(frog, "Freeze_compare.csv") #checking freeze to DT
+frog$freeze <- get_freeze(low_thresh_temp, frog$tmean, high_thresh_temp)
 
 # Get rain
 get_rain = function(freeze, precip){
@@ -59,6 +59,7 @@ get_melt = function(tmean, low_thresh_temp, pack, hock, p.0=NULL){
                             p.i, (tmean[i]-low_thresh_temp)*hock[i]))
     p.i = pack[i]
   }
+  
   return(melt)
 }
 
@@ -77,6 +78,25 @@ get_pack = function(snow, melt, p.0=NULL){
 
 frog$pack <- get_pack(frog$snow, frog$melt, p.0=NULL)
 
+#--------------------------------------------------------------------------------#
+#adding melt and pack loops together
+get_pack_melt = function(tmean,low_thresh_temp, hock, snow, p.0=NULL){
+    p.i = ifelse(!is.null(p.0), p.0, 0)
+    melt=c()
+    pack=c()
+    for(i in 1:length(tmean)){
+      melt[i] = ifelse(tmean[i]<low_thresh_temp||p.i==0, 0, 
+                       ifelse((tmean[i]-low_thresh_temp)*hock[i]>p.i, 
+                              p.i, (tmean[i]-low_thresh_temp)*hock[i]))
+      pack[i] = p.i+snow[i]-melt[i]
+        p.i=pack[i]
+    }
+    return(melt)
+    return(pack)
+}      
+        
+frog$pack <- get_pack_melt(frog$tmean, low_thresh_temp, frog$Hock, frog$snow, p.0=NULL)
+frog$melt <- get_pack_melt(frog$tmean, low_thresh_temp, frog$Hock, frog$snow, p.0=NULL)
 
 ## Write as csv to compare to DT and MT
 write.csv(frog_input, "C:/Users/msears/OneDrive - DOI/WB-cross check/SWE_output")
