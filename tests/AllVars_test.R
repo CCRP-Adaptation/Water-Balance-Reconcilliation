f25216da776a1d630810cc02f81acd51ec44fa86
msears <megan_sears@partner.nps.gov>
2021-02-25 17:08:14 -0700

    2/25/21 finished Oudin PET

--- tests/AllVars_test.R
+++ tests/AllVars_test.R
@@ -64,7 +64,7 @@ get_melt = function(tmean,low_thresh_temp, hock, snow, pack=0){
 
 frog$melt <- get_melt(frog$tmean, low_thresh_temp, frog$Hock, frog$snow, pack=0)
 
-# Get pack - correspond with 'PACK' in D. Thoma v2 model
+# Get pack - corresponds with 'PACK' in D. Thoma v2 model
 get_pack = function(tmean,low_thresh_temp, hock, snow, pack=0){
   melt <- vector()
   pack <- 0 #this is the init value
@@ -81,11 +81,6 @@ get_pack = function(tmean,low_thresh_temp, hock, snow, pack=0){
 
 frog$pack <- get_pack(frog$tmean, low_thresh_temp, frog$Hock, frog$snow, pack=0)
 
-## Write as csv to compare (rain, snow, pack, melt) to DT and MT before moving on
-write.csv(frog, "C:/Users/msears/OneDrive - DOI/WB_crosscheck/firstcheck.csv")
-
-#rain, snow, pack, melt agree with D.Thoma v2 model
-
 # W (water reaching soil as rain + melt) - rain + melt
 get_W = function(rain, melt){
   W = (melt+rain)
@@ -95,34 +90,33 @@ get_W = function(rain, melt){
 frog$W <- get_W(frog$rain, frog$melt)
 
 # Oudin PET (Oudin location * heatload * PET shade coeff) - corresponds with 'PET' in D. Thoma v2 model
+#!!!!!!!# use this Oudin PET function in the WB model since it actually uses pi
+#get_OudinPET = function(doy, lat, pack, tmean, slope, aspect, shadecoeff=1){
+#  d.r = 1 + 0.033*cos((2*pi/365)*doy)
+#  declin = 0.409*sin((((2*pi)/365)*doy)-1.39)
+#  lat.rad = (pi/180)*lat
+#  sunset.ang = acos(-tan(lat.rad)*tan(declin))
+#  R.a = ((24*60)/pi)*0.082*d.r*((sunset.ang*sin(lat.rad)*sin(declin)) + (cos(lat.rad)*cos(declin)*sin(sunset.ang)))
+#  Oudin = ifelse(pack>2,0,ifelse(tmean>-5,(R.a*(tmean+5)*0.408)/100,0))
+#  Folded_aspect = abs(180-abs((aspect)-225))
+#  Heatload = (0.339+0.808*cos(deg2rad(lat))*cos(deg2rad(slope)))-(0.196*sin(deg2rad(lat))*sin(deg2rad(slope)))-(0.482*cos(deg2rad(Folded_aspect))*sin(deg2rad(slope)))
+#  OudinPET = Oudin * Heatload * shadecoeff
+#  return(OudinPET)
+#}
+
+#frog$PET <- get_OudinPET(frog$yday, latitude, frog$pack, frog$tmean, frog$slope, frog$aspect, shadecoeff=1)
+
 latitude=44.95354
 
+#!! this is the same function as above EXCEPT it usues 3.14 or 3.14519 instead of pi in order to replciate what Thoma did
 get_OudinPET = function(doy, lat, pack, tmean, slope, aspect, shadecoeff=1){
-  d.r = 1 + 0.033*cos(((2*pi)/365)*doy)
-  declin = 0.409*sin((((2*pi)/365)*doy)-1.39)
-  lat.rad = (pi/180)*lat
-  sunset.ang = acos(-tan(lat.rad)*tan(declin))
-  R.a = ((24*60)/pi)*0.0820*d.r*(sunset.ang*sin(lat.rad)*sin(declin) + cos(lat.rad)*cos(declin)*sin(sunset.ang))
-  Oudin = ifelse(pack>2,0,ifelse(tmean>-5,(R.a*(tmean+5)*0.405)/100,0))
-  Folded_aspect = abs(180-abs((slope)-225))
-  Heatload = (0.339+0.808*cos(deg2rad(lat))*cos(deg2rad(slope)))-(0.196*sin(deg2rad(lat))*sin(deg2rad(slope)))-(0.482*cos(deg2rad(Folded_aspect))*sin(deg2rad(slope)))
-  OudinPET = Oudin* Heatload * shadecoeff
-  return(OudinPET)
-}
-
-frog$PET <- get_OudinPET(frog$yday, latitude, frog$pack, frog$tmean, frog$slope, frog$aspect, shadecoeff=1)
-
-# troubleshooting PET
-latitude=44.95354
-
-get_OudinPET = function(doy, lat, pack, tmean, slope, aspect, shadecoeff=1){
-  d.r = 1 + 0.033*cos(((2*3.14159)/365)*doy)
+  d.r = 1 + 0.033*cos((2*3.14159/365)*doy)
   declin = 0.409*sin((((2*3.14)/365)*doy)-1.39)
   lat.rad = (3.14159/180)*lat
   sunset.ang = acos(-tan(lat.rad)*tan(declin))
   R.a = ((24*60)/3.14159)*0.082*d.r*((sunset.ang*sin(lat.rad)*sin(declin)) + (cos(lat.rad)*cos(declin)*sin(sunset.ang)))
-  Oudin = ifelse(pack>2,0,ifelse(tmean>-5,((R.a*(tmean+5)*0.408))/100,0))
-  Folded_aspect = abs(180-abs((slope)-225))
+  Oudin = ifelse(pack>2,0,ifelse(tmean>-5,(R.a*(tmean+5)*0.408)/100,0))
+  Folded_aspect = abs(180-abs((aspect)-225))
   Heatload = (0.339+0.808*cos(deg2rad(lat))*cos(deg2rad(slope)))-(0.196*sin(deg2rad(lat))*sin(deg2rad(slope)))-(0.482*cos(deg2rad(Folded_aspect))*sin(deg2rad(slope)))
   OudinPET = Oudin * Heatload * shadecoeff
   return(OudinPET)
@@ -130,7 +124,14 @@ get_OudinPET = function(doy, lat, pack, tmean, slope, aspect, shadecoeff=1){
 
 frog$PET <- get_OudinPET(frog$yday, latitude, frog$pack, frog$tmean, slope=2, frog$aspect, shadecoeff=1)
 
-# W - PET
+# W - PET- corresponds with 'W-PET' in D. Thoma v2 model
+get_W_PET = function(W, PET){
+  W_PET = (W-PET)
+  return(W_PET)
+}
+
+frog$W_PET <- get_W_PET(frog$W, frog$PET)
+
 
 # Soil (inital value of 104)
 
@@ -143,4 +144,7 @@ frog$PET <- get_OudinPET(frog$yday, latitude, frog$pack, frog$tmean, slope=2, fr
 # Deficit (PET-AET)
 
 #GDD (based off tmean and Tbase constant[0])
+
+## Write as csv to compare to DT and MT
+write.csv(frog, "C:/Users/msears/OneDrive - DOI/WB_crosscheck/frog_check.csv")
 
