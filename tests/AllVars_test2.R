94fbce3a13bf1f55b9e2d417e0dc1d436fdee5f9
msears <megan_sears@partner.nps.gov>
2021-03-22 13:57:56 -0600

    testing function using package

--- tests/AllVars_test2.R
+++ tests/AllVars_test2.R
@@ -4,8 +4,6 @@ library(dplyr)
 
 ## Input and prep
 
-rm(list = ls())
-
 setwd("C:/Users/msears/OneDrive - DOI/WB_crosscheck/")
 
 frog <- read.csv("C:/Users/msears/OneDrive - DOI/WB_crosscheck/FrogRock-inputforR.csv")
@@ -19,4 +17,18 @@ Lon <- -110.54083
 ## Test functions from WB v2 package
 
 frog$jtemp <- get_jtemp(Lat, Lon)
+
+frog$freeze <- get_freeze(frog$jtemp, frog$tmean)
+
+frog$rain <- get_rain(frog$precip_mmday,frog$freeze)
+
+frog$snow <- get_snow(frog$precip_mmday, frog$freeze)
+
+frog$melt <- get_melt(frog$tmean, frog$jtemp, frog$Hock, frog$snow)
+
+frog$snowpack <- get_snowpack(frog$jtemp, frog$snow, frog$melt)
+
+frog$W <- get_w(frog$rain, frog$melt)
+
+frog$PET <- get_OudinPET(frog$d)
 
